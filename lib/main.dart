f9f2b9262eb805701ad0bb60d75eaa3d774115d7
trykea <tikearith@gmail.com>
2024-11-19 16:52:02 +0700

    Added new transaction as a modal

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-
 import './models/new_transaction.dart';
 import './widgets/transaction_list.dart';
-import './models/user_transactions.dart';
+import 'models/transaction.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
+
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter App',
@@ -17,21 +17,44 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-
-  // String titleInput='';
-  // String amountInput='';
-
-
+class MyHomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(id: '1', title: 'Item1', amount: 9.99, date: DateTime.now()),
+    Transaction(id: '2', title: 'Item2', amount: 19.99, date: DateTime.now()),
+  ];
+  void _addNewTransaction(String addTitle,double addAmount) {
+    final newTx = Transaction(
+        title: addTitle,
+        amount: addAmount,
+        date: DateTime.now(),
+        id: DateTime.now().toString());
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });}
+    void _openNewTransactionModal(BuildContext ctx) {
+      showModalBottomSheet(context: ctx, builder: (bCtx) {
+        return NewTransaction(_addNewTransaction);
+      });
+    }
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue,
           title: Text(
             'Flutter App',
             style: TextStyle(color: Colors.white),
           ),
+          actions: [
+            IconButton(
+              onPressed: () => _openNewTransactionModal(context), icon: Icon(Icons.add), color: Colors.white,)
+          ],
         ),
         body: SingleChildScrollView(
           child: Column(
@@ -45,10 +68,22 @@ class MyHomePage extends StatelessWidget {
                   elevation: 5,
                 ),
               ),
-              UserTransactions(),
-          
+              TransactionList(_userTransactions),
             ],
           ),
-        ));
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton(child:
+        Icon(Icons.add,color: Colors.white,),
+          backgroundColor: Colors.blue,
+          shape: RoundedRectangleBorder( // Example: Rounded edges
+            borderRadius: BorderRadius.circular(40.0),
+          ),
+          onPressed: () => _openNewTransactionModal(context),
+
+        )
+        ,
+      );
+    }
   }
-}
+
