c02431ce828f06342baedfebc517d38a8c3ea30a
trykea <tikearith@gmail.com>
2024-11-26 15:05:17 +0700

    fixed error 2

--- lib/widgets/adaptive_text_button.dart
+++ lib/widgets/adaptive_text_button.dart
@@ -6,7 +6,8 @@ class AdaptiveTextButton extends StatelessWidget {
   final String text;
   final VoidCallback handler;
 
-  AdaptiveTextButton(this.text,this.handler)
+  AdaptiveTextButton(this.text,this.handler);
+
   @override
   Widget build(BuildContext context) {
     return  Platform.isIOS
