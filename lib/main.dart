112619836127b4955cd9853a1309cd195e9523b4
trykea <tikearith@gmail.com>
2024-11-26 14:02:51 +0700

    Use cupertino appbar,theme,floatingIcon,body

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
 import './models/new_transaction.dart';
@@ -18,42 +20,71 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Personal Expenses',
-      // theme: ThemeData(
-      //   primarySwatch: Colors.red,
-      //   primaryColor: Colors.red,
-      //   hintColor: Colors.blue,
-      // ),
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.blue,
-        ).copyWith(
-          secondary: Colors.red, // Replacement for accentColor
-        ),
-        fontFamily: 'QuickSand',
-        textTheme: ThemeData.light().textTheme.copyWith(
-              headlineSmall: TextStyle(
-                fontFamily: 'OpenSans',
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-              ),
-              labelLarge: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-        appBarTheme: AppBarTheme(
-          titleTextStyle: TextStyle(
-            fontFamily: 'OpenSans',
-            fontSize: 20,
-            fontWeight: FontWeight.bold, // Optional: Add weight if needed
-            color: Colors.white, // Optional: Define a color for the text
-          ),
-        ),
-      ),
-      home: MyHomePage(),
+    return Platform.isIOS
+        ? CupertinoApp(
+    title: 'Personal Expenses',
+    theme: CupertinoThemeData(
+    primaryColor: CupertinoColors.systemBlue, // Equivalent to primarySwatch
+    barBackgroundColor: CupertinoColors.systemRed, // Equivalent to secondary color
+    textTheme: CupertinoTextThemeData(
+    textStyle: TextStyle(
+    fontFamily: 'QuickSand',
+    color: CupertinoColors.black, // Default text color
+    )
+    ,
+    navTitleTextStyle: TextStyle(
+    fontFamily: 'OpenSans',
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: CupertinoColors.white,
+    ),
+    actionTextStyle: TextStyle(
+    fontFamily: 'OpenSans',
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: CupertinoColors.white,
+    ),
+    ),
+    ),
+    home: MyHomePage(),
+    )
+        : MaterialApp(
+    title: 'Personal Expenses',
+    // theme: ThemeData(
+    //   primarySwatch: Colors.red,
+    //   primaryColor: Colors.red,
+    //   hintColor: Colors.blue,
+    // ),
+    theme: ThemeData(
+    colorScheme: ColorScheme.fromSwatch(
+    primarySwatch: Colors.blue,
+    ).copyWith(
+    secondary: Colors.red, // Replacement for accentColor
+    ),
+    fontFamily: 'QuickSand',
+    textTheme: ThemeData.light().textTheme.copyWith(
+    headlineSmall: TextStyle(
+    fontFamily: 'OpenSans',
+    fontWeight: FontWeight.bold,
+    fontSize: 18,
+    ),
+    labelLarge: TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+    ),
+    ),
+    appBarTheme: AppBarTheme(
+    titleTextStyle: TextStyle(
+    fontFamily: 'OpenSans',
+    fontSize: 20,
+    fontWeight: FontWeight.bold, // Optional: Add weight if needed
+    color: Colors.white, // Optional: Define a color for the text
+    ),
+    ),
+    ),
+    home: MyHomePage()
+    ,
     );
   }
 }
@@ -80,8 +111,8 @@ class _MyHomePageState extends State<MyHomePage> {
     }).toList();
   }
 
-  void _addNewTransaction(
-      String addTitle, double addAmount, DateTime chosenDate) {
+  void _addNewTransaction(String addTitle, double addAmount,
+      DateTime chosenDate) {
     final newTx = Transaction(
         title: addTitle,
         amount: addAmount,
@@ -109,9 +140,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
-    final appBar = AppBar(
-      backgroundColor: Theme.of(context).colorScheme.primary,
+    final mediaQuery = MediaQuery.of(context);
+    final isLandscape = mediaQuery.orientation == Orientation.landscape;
+    final AndroidAppBar = AppBar(
+      backgroundColor: Theme
+          .of(context)
+          .colorScheme
+          .primary,
       title: Text(
         'Personal Expenses',
         style: TextStyle(color: Colors.white),
@@ -124,60 +159,90 @@ class _MyHomePageState extends State<MyHomePage> {
         )
       ],
     );
+    final iosAppBar = CupertinoNavigationBar(
+      middle: Text(
+        'Personal Expenses',
+        style: TextStyle(color: Colors.white),
+      ),
+      trailing: CupertinoButton(
+        padding: EdgeInsets.zero,
+        // Removes default padding for a compact button
+        onPressed: () => _openNewTransactionModal(context),
+        child: Icon(
+          CupertinoIcons.add, // Cupertino-style "add" icon
+          color: CupertinoColors.white,
+        ),
+      ),
+    );
     final txListWidget = Container(
-        height: (MediaQuery.of(context).size.height -
-            appBar.preferredSize.height -
-            MediaQuery.of(context).padding.top) *
+        height: (mediaQuery.size.height -
+            AndroidAppBar.preferredSize.height -
+            mediaQuery.padding.top) *
             0.7,
-        child:
-        TransactionList(_userTransactions, _deleteTransaction));
-    return Scaffold(
-      appBar: appBar,
-      body: SingleChildScrollView(
+        child: TransactionList(_userTransactions, _deleteTransaction));
+    final pagebody = SafeArea(
+      child: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            if (isLandscape) Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Show chart'),
-                Switch(
-                    value: _showChart,
-                    inactiveThumbColor: Colors.white,
-                    inactiveTrackColor: Colors.grey,
-                    onChanged: (val) {
-                      setState(() {
-                        _showChart = val;
-
-                      });
-                    })
-              ],
-            ),
-            if (!isLandscape) Container(
-                height: (MediaQuery.of(context).size.height -
-                    appBar.preferredSize.height -
-                    MediaQuery.of(context).padding.top) *
-                    0.3,
-                child: Chart(_recentTransactions)),
+            if (isLandscape)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Show chart'),
+                  Switch.adaptive(
+                      value: _showChart,
+                      inactiveThumbColor: Colors.white,
+                      inactiveTrackColor: Colors.grey,
+                      onChanged: (val) {
+                        setState(() {
+                          _showChart = val;
+                        });
+                      })
+                ],
+              ),
+            if (!isLandscape)
+              Container(
+                  height: (mediaQuery.size.height -
+                      AndroidAppBar.preferredSize.height -
+                      mediaQuery.padding.top) *
+                      0.3,
+                  child: Chart(_recentTransactions)),
             if (!isLandscape) txListWidget,
-            if  (isLandscape) _showChart
-                ? Container(
-                    height: (MediaQuery.of(context).size.height -
-                            appBar.preferredSize.height -
-                            MediaQuery.of(context).padding.top) *
-                        0.7,
-                    child: Chart(_recentTransactions))
-                : txListWidget,
+            if (isLandscape)
+              _showChart
+                  ? Container(
+                  height: (mediaQuery.size.height -
+                      AndroidAppBar.preferredSize.height -
+                      mediaQuery.padding.top) *
+                      0.7,
+                  child: Chart(_recentTransactions))
+                  : txListWidget,
           ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
+    );
+    return Platform.isIOS
+        ? CupertinoPageScaffold(
+      child: pagebody,
+      navigationBar: iosAppBar,
+    )
+        : Scaffold(
+      appBar: AndroidAppBar,
+      body: pagebody,
+      floatingActionButtonLocation:
+      FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Platform.isIOS
+          ? Container()
+          : FloatingActionButton(
         child: Icon(
           Icons.add,
           color: Colors.white,
         ),
-        backgroundColor: Theme.of(context).colorScheme.primary,
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .primary,
         shape: RoundedRectangleBorder(
           // Example: Rounded edges
           borderRadius: BorderRadius.circular(40.0),
